a363c9ff5ffad665b22ec59ef1a30474a050b5b7
Don Chia <chia.qien.don@dhs.sg>
2020-12-02 02:49:43 +0800

    photos page

--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="vstack">
-    <div class="hstack">
-      <div class="my-96"><h1>Photos</h1></div>
+  <div class="vstack w-full">
+    <div class="container">
+      <div class="vstack my-96 align-start w-full px-3">
+        <h1 class="large-title">Film Photography?</h1>
+        <h1 class="large-title">Yea I like making things unnecessarily difficult for myself.</h1>
+        <h3 class="small-content">SLR: 1979 Yashica FX-7</h3>
+        <h3 class="small-content">Rangefinder: 1959 FED-2</h3>
+        <h3 class="small-content">Film: Kodak Gold 200, Kodak ColorPlus 200, Kodak Portra 400/800, Fuji Velvia 100, Fujifilm Pro 400H</h3>
+      </div>
     </div>
     <!--div class="image-grid p-8">
       <img  src="../static/Analog1.jpg" />
@@ -14,62 +20,162 @@
       <img  src="../static/Analog8.jpg" />
       <img  src="../static/Analog9.jpg" />
     </div-->
-    <div class="grid gap-8 p-8" style="grid-template-columns: repeat(auto-fill, minmax(448px, 1fr))">
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1593642532400-2682810df593?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1606757331120-274906567028?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1601758174039-617983b8cdd9?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1886&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1593642532400-2682810df593?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1606757331120-274906567028?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1601758174039-617983b8cdd9?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1886&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1593642532400-2682810df593?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1606757331120-274906567028?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1601758174039-617983b8cdd9?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1886&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1593642532400-2682810df593?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1606757331120-274906567028?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1601758174039-617983b8cdd9?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1886&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1593642532400-2682810df593?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1606757331120-274906567028?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1601758174039-617983b8cdd9?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1886&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1593642532400-2682810df593?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1606757331120-274906567028?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80">
-	</div>
-	<div style="--aspect-ratio: 16 / 9">
-		<img class="radius-4 shadow-sm" src="https://images.unsplash.com/photo-1601758174039-617983b8cdd9?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1886&q=80">
-	</div>
-</div>
+    <div class="align-self-stretch grid gap-8 sm:p-8" style="grid-template-columns: repeat(auto-fill, minmax(448px, 1fr))">
+      <div style="--aspect-ratio: 16 / 9" class="w-full">
+        <img class="shadow-sm" src="../static/Analog1.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9" class="w-full">
+        <img class="shadow-sm" src="../static/Analog2.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9" class="w-full">
+        <img class="shadow-sm" src="../static/Analog3.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9" class="w-full">
+        <img class="shadow-sm" src="../static/Analog4.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9" class="w-full">
+        <img class="shadow-sm" src="../static/Analog5.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9" class="w-full">
+        <img class="shadow-sm" src="../static/Analog6.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog7.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog8.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog9.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog10.jpg">
+      </div>
+      <!--div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog11.jpg">
+      </div-->
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog12.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog13.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog14.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog15.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog16.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog17.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog18.jpg">
+      </div>
+      <!--div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog19.jpg">
+      </div-->
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog20.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog44.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog45.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog46.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog47.jpg">
+      </div>
+      <div style="--aspect-ratio: 4 / 3">
+        <img class="shadow-sm" src="../static/Analog48.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog49.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog50.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog51.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog21.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog22.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog23.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog24.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog25.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog26.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog27.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog28.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog29.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog30.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog31.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog32.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog33.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog34.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog35.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog36.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog37.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog38.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog39.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog40.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog41.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog42.jpg">
+      </div>
+      <div style="--aspect-ratio: 16 / 9">
+        <img class="shadow-sm" src="../static/Analog43.jpg">
+      </div>
+      
+    </div>
   </div>
 </template>
 
@@ -94,5 +200,15 @@ export default {
 
 img {
 	object-fit: cover;
+}
+
+.large-title {
+  font-weight: 400;
+  
+}
+
+.small-content {
+  font-weight: 300;
+  font-size: 15px;
 }
 </style>
