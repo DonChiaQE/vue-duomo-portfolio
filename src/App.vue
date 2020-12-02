7fb6742417a630a3a8d5dc6e07297b8f278798ea
Don Chia <chia.qien.don@dhs.sg>
2020-12-02 18:36:42 +0800

    missed something

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" !data-debug data-debug-space>
+  <div id="app">
     <!--div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
